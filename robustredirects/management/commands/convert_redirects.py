e99672d7d5f93ce0ab578f08320f55c4a35aa2d5
Cezar Jenkins <emperorcezar@gmail.com>
2013-10-09 21:53:20 -0500

    Django can't use a .create on models with unique Wrap our conversion in a transaction

--- robustredirects/management/commands/convert_redirects.py
+++ robustredirects/management/commands/convert_redirects.py
@@ -1,16 +1,16 @@
 from django.core.management.base import BaseCommand, CommandError
 from robustredirects.models import Redirect
 from django.contrib.redirects.models import Redirect as DjangoRedirect
-
+from django.db import transaction
 
 class Command(BaseCommand):
     can_import_settings = True
 
     def handle(self, *args, **options):
-        count = 0
-        for redirect in DjangoRedirect.objects.all():
-            Redirect.objects.create(from_url=redirect.old_path, to_url=redirect.new_path, site=redirect.site,
-                                    http_status=301)
-            count += 1
+        with transaction.commit_on_success():
+            count = 0
+            for redirect in DjangoRedirect.objects.all():
+                Redirect(from_url=redirect.old_path, to_url=redirect.new_path, site=redirect.site, http_status=301)
+                count += 1
 
         print "Copied {} number of redirects into robust redirects."
