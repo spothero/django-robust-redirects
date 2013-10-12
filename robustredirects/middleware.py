200157daa22dfbde46c7f8952667b81f08e81b29
Cezar Jenkins <emperorcezar@gmail.com>
2013-10-11 22:36:38 -0500

    Refactor out some reused code. To a direct redirect for redirects that aren't regex. Use django's function to get the current site.

--- robustredirects/middleware.py
+++ robustredirects/middleware.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.sites.models import get_current_site
 from django.core.urlresolvers import resolve, Resolver404
 from django.http import HttpResponsePermanentRedirect, HttpResponseRedirect, HttpResponseGone
 from robustredirects.models import Redirect
@@ -9,39 +10,78 @@ class RedirectMiddleware(object):
     """
         Process the redirect patterns from redirects.dynamic_urls.
     """
+
+    def try_resolve(self, path, request):
+        urlconf = 'robustredirects.dynamic_urls'
+        redirect, args, kwargs = resolve(path, urlconf=urlconf)
+        return redirect(request, **kwargs)
+
     def process_response(self, request, response):
         if response.status_code != 404:
             # No need to check for a redirect for non-404 responses.
             return response
 
         path = request.get_full_path()
-
         try:
-            urlconf = 'robustredirects.dynamic_urls'
-            redirect, args, kwargs = resolve(path, urlconf=urlconf)
-            return redirect(request, **kwargs)
+            return self.try_resolve(path, request)
         except Resolver404:
-            # No redirect was found. Try looking for a partial
-            site_id = settings.SITE_ID
-            db_filters = {
-                'status': 1,
-                'site': site_id,
-                'is_partial': True
-            }
+            pass
 
-            redirects = Redirect.objects.filter(**db_filters)
+        # Try again by changing the slash
+        try:
+            # if not, check if adding/removing the trailing slash helps
+            if path.endswith('/'):
+                new_path = path[:-1]
+            else:
+                new_path = path + '/'
 
-            for redirect in redirects:
-                if redirect.from_url in path:
-                    if redirect.to_url == '':
-                        return HttpResponseGone()
+            return self.try_resolve(new_path, request)
+        except Resolver404:
+            pass
 
-                    # Do a replace on the url and do a redirect
-                    path = replace_partial_url(path, redirect.from_url, redirect.to_url)
 
-                    if redirect.http_status == 301:
-                        return HttpResponsePermanentRedirect(path)
-                    elif redirect.http_status == 302:
-                        return HttpResponseRedirect(path)
+        current_site = get_current_site(request)
 
-            return response
+        # No regex redirect was found try a simple replace
+        db_filters = {
+            'status': 1,
+            'site': current_site,
+            'is_partial': False,
+            'uses_regex': False
+        }
+
+        redirects = Redirect.objects.filter(**db_filters)
+
+        for redirect in redirects:
+            if redirect.from_url.lower() == path.lower():
+                if redirect.to_url == '':
+                    return HttpResponseGone()
+
+                if redirect.http_status == 301:
+                    return HttpResponsePermanentRedirect(redirect.to_url)
+                elif redirect.http_status == 302:
+                    return HttpResponseRedirect(redirect.to_url)
+
+        # Try looking for a partial
+        db_filters = {
+            'status': 1,
+            'site': current_site,
+            'is_partial': True
+        }
+
+        redirects = Redirect.objects.filter(**db_filters)
+
+        for redirect in redirects:
+            if redirect.from_url in path:
+                if redirect.to_url == '':
+                    return HttpResponseGone()
+
+                # Do a replace on the url and do a redirect
+                path = replace_partial_url(path, redirect.from_url, redirect.to_url)
+
+                if redirect.http_status == 301:
+                    return HttpResponsePermanentRedirect(path)
+                elif redirect.http_status == 302:
+                    return HttpResponseRedirect(path)
+
+        return response
