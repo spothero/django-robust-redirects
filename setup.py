7257aaf79bde9fe86a10812bb82bd8a64ccec3fc
Cezar Jenkins <emperorcezar@gmail.com>
2013-10-14 10:47:54 -0500

    Mark a new release.

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ import os
 
 setup(
     name = "django-robust-redirects",
-    version = "0.9",
+    version = "0.9.1",
     url = 'http://github.com/spothero/django-robust-redirects',
-    download_url = 'http://github.com/spothero/django-robust-redirects/tarball/0.9',
+    download_url = 'http://github.com/spothero/django-robust-redirects/tarball/0.9.1',
     description = "A more robust and feature full django redirect package",
     author = 'SpotHero and Glen Zangirolami',
     author_email = 'cezar@spothero.com',
