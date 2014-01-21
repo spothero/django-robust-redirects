01da4cdbbe508435807b00e3a053332ec0a36fad
Cezar Jenkins <emperorcezar@gmail.com>
2014-01-21 11:50:01 -0600

    Finish updating our setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@
 from setuptools import setup
 
 setup(
-    name = "django-robust-redirects",
-    version = "0.9.2",
-    url = 'http://github.com/spothero/django-robust-redirects',
-    download_url = 'http://github.com/spothero/django-robust-redirects/tarball/0.9.1',
-    description = "A more robust and feature full django redirect package",
-    author = 'SpotHero and Glen Zangirolami',
-    author_email = 'cezar@spothero.com',
-    packages = ['robustredirects'],
-    include_package_data = True,
-    zip_safe = False,
-    classifiers = [
+    name="django-robust-redirects",
+    version="0.9.2",
+    url='http://github.com/spothero/django-robust-redirects',
+    download_url='http://github.com/spothero/django-robust-redirects/tarball/0.9.2',
+    description="A more robust and feature full django redirect package",
+    author='SpotHero and Glen Zangirolami',
+    author_email='cezar@spothero.com',
+    packages=['robustredirects'],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
