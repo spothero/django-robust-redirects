dff9b11aa46b8104ee8d523969931324b1fdf4be
Adam "Cezar" Jenkins <emperorcezar@gmail.com>
2015-05-14 12:31:58 -0500

    Push the version for setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="django-robust-redirects",
-    version="0.9.2",
+    version="0.9.3",
     url='http://github.com/spothero/django-robust-redirects',
     download_url='http://github.com/spothero/django-robust-redirects/tarball/0.9.2',
     description="A more robust and feature full django redirect package",
