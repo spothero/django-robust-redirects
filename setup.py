f771ec2d227ed25776939f65d4fc085b0cc82971
Adam "Cezar" Jenkins <emperorcezar@gmail.com>
2015-05-14 12:33:06 -0500

    Bump up the download url version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="django-robust-redirects",
     version="0.9.3",
     url='http://github.com/spothero/django-robust-redirects',
-    download_url='http://github.com/spothero/django-robust-redirects/tarball/0.9.2',
+    download_url='http://github.com/spothero/django-robust-redirects/tarball/0.9.3',
     description="A more robust and feature full django redirect package",
     author='SpotHero and Glen Zangirolami',
     author_email='cezar@spothero.com',
