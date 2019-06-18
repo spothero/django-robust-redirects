cc4580e05f3dee0c1951c68ba5dde4612c2391a3
Nicholas Kincaid <nbkincaid@gmail.com>
2019-06-18 15:43:18 -0500

    PTP3-6 bump version to 0.12.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="django-robust-redirects",
-    version="0.11.0",
+    version="0.12.0",
     url="http://github.com/spothero/django-robust-redirects",
     download_url="http://github.com/spothero/django-robust-redirects/tarball/0.10.0",
     description="A more robust and feature full django redirect package",
