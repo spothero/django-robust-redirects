eb23b159b45f052976d7a071c51389b08cd34995
Nicholas Kincaid <nbkincaid@gmail.com>
2019-06-20 11:50:29 -0500

    PTP3-6 constrain dependencies and bump version to 0.13.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="django-robust-redirects",
-    version="0.12.0",
+    version="0.13.0",
     url="http://github.com/spothero/django-robust-redirects",
     download_url="http://github.com/spothero/django-robust-redirects/tarball/0.10.0",
     description="A more robust and feature full django redirect package",
@@ -22,5 +22,9 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
     ],
-    requires=["django", "future", "six"],
+    requires=[
+        "django>=1.10.3,<1.11.0",
+        "future>=0.17.1,<0.18.0",
+        "six>=1.12.0,<1.13.0"
+    ],
 )
