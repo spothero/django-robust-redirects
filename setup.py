f5f8ffa7828c0354cd7b9565ceb4991bae393231
Larry Kiss <larry@spothero.com>
2021-11-23 12:39:12 -0800

    Temporarily support Django 3.0 and 3.1.

--- setup.py
+++ setup.py
@@ -24,7 +24,8 @@ setup(
     ],
     python_requires=">=3.6",
     install_requires=[
-        "django>=3.2,<4",
+        # "django>=3.2,<4",  # TODO use this going forward.  The other is just temporary.
+        "django>=3,<4",
         "six>=1.12.0,<2"
     ],
 )
